77f2265caf36ec37d935baa3b785548b086cce6b
Valentin Evteev <stratumforce@protonmail.com>
2020-04-09 15:00:24 +0300

    refactor: restructure submit controller

--- src/js/controllers/submit.js
+++ src/js/controllers/submit.js
@@ -2,56 +2,58 @@ import _ from 'lodash';
 
 import get from '../lib/feed';
 
-const isAvailableUrl = ({ feedForm, feeds }) => {
-  const { value: url } = feedForm;
-  const { channels } = feeds;
+const setFormState = (state, newState) => _.merge(state.feedForm, newState);
+const lock = (state) => setFormState(state, { state: 'send' });
+const unlock = (state) => setFormState(state, { state: 'input' });
+const invalidateForm = (state) => setFormState(state, { isValid: false });
+const clear = (state) => setFormState(state, { value: '' });
 
-  const isAvailable = _.filter(channels, { originURL: url }).length === 0;
+const pushError = (state, msg) => {
+  const { feedForm } = state;
 
-  return isAvailable;
+  setFormState(state, {
+    state: 'error',
+    errors: [...feedForm.errors, msg],
+  });
 };
+
+const isAvailableUrl = (url, channels) => !_.some(channels, { originURL: url });
 
 const getFeed = ({ feedForm }) => get(feedForm.value);
 
-const clear = (state) => {
-  const { feedForm } = state;
-  feedForm.value = '';
+const addFeed = (state, feed) => {
+  const { feeds } = state;
+
+  const channel = {
+    ...feed.channel,
+    originURL: feed.originURL,
+    isActive: false,
+  };
+
+  feeds.channels = [...feeds.channels, channel];
+  feeds.items = [...feeds.items, ...feed.items];
 };
 
-const lock = (state) => {
-  const { feedForm } = state;
-  feedForm.state = 'send';
-};
+const processInput = (state) => {
+  const { feedForm, feeds } = state;
+  const { value: url } = feedForm;
 
-const unlock = (state) => {
-  const { feedForm } = state;
-  feedForm.state = 'input';
+  const isAvailable = isAvailableUrl(url, feeds.channels);
+
+  if (isAvailable) {
+    lock(state);
+    getFeed(state)
+      .then((feed) => addFeed(state, { ...feed, originURL: url }))
+      .then(() => clear(state))
+      .then(() => unlock(state))
+      .catch((err) => pushError(state, err.message));
+  } else {
+    invalidateForm(state);
+    pushError(state, 'Feed has been already added');
+  }
 };
 
 export default (event, state) => {
   event.preventDefault();
-
-  const { feedForm, feeds } = state;
-
-  if (!isAvailableUrl(state)) {
-    feedForm.isValid = false;
-    feedForm.state = 'error';
-
-    const error = 'Feed has been already added';
-    feedForm.errors = [...feedForm.errors, error];
-
-    return null;
-  }
-
-  lock(state);
-
-  return getFeed(state)
-    .then((data) => {
-      const channel = { ...data.channel, originURL: feedForm.value };
-
-      feeds.channels = [...feeds.channels, channel];
-      feeds.items = [...feeds.items, ...data.items];
-    })
-    .then(() => clear(state))
-    .finally(() => unlock(state));
+  processInput(state);
 };
