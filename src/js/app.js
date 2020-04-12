0fbf24b1b46507b0311d41b3327acec6567f880c
Valentin Evteev <stratumforce@protonmail.com>
2020-04-12 14:35:33 +0300

    refactor: import getForm in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,10 +3,11 @@ import resources from './locales';
 
 import initialState from './model/state';
 import runWatchers from './view/watchers';
+import { getForm } from './view/components/util';
 import { inputController, submitController } from './controllers';
 
 const setEvents = (state) => {
-  const form = document.forms['frm-feed'];
+  const form = getForm();
 
   form.addEventListener('submit', (event) => submitController(event, state));
   form.addEventListener('input', (event) => inputController(event, state));
