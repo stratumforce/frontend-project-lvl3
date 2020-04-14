237ec402c6fc9afc78a33c59c6b7f37ddc105641
Valentin Evteev <stratumforce@protonmail.com>
2020-04-14 15:54:25 +0300

    refactor: rename components to mapping in render.js

--- src/js/view/render.js
+++ src/js/view/render.js
@@ -2,14 +2,14 @@ import renderFeedForm from './components/form';
 import renderFeeds from './components/feeds';
 import renderAlert from './components/alert';
 
-const components = {
+const mapping = {
   feedForm: renderFeedForm,
   feeds: renderFeeds,
   alert: renderAlert,
 };
 
 export default (state, component) => {
-  const renderFn = components[component];
+  const renderFn = mapping[component];
 
   renderFn(state);
 };
